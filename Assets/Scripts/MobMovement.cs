ce44ac55c0ab5abd78e6e2de0490d92587d70ea4
triple23 <72692317+triple23@users.noreply.github.com>
2021-02-05 16:50:18 +0900

    몹 이동 스크립트 적용

--- Assets/Scripts/MobMovement.cs
+++ Assets/Scripts/MobMovement.cs
@@ -7,7 +7,7 @@ public class MobMovement : MonoBehaviour
     float rightMax = 2.0f; //좌로 이동가능한 (x)최대값
     float leftMax = -2.0f; //우로 이동가능한 (x)최대값
     float currentPosition; //현재 위치(x) 저장
-    float direction = 2.0f; //이동속도+방향
+    float direction = 1.0f; //이동속도+방향
 
     void Start()
 
